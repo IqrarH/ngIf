7152fa8e984562442a3820968c68f9bb131cd53d
IqrarH <iqrarhussain471@gmail.com>
2021-08-11 14:26:51 +0500

    Initial Commit

---
+++ src/app/ngif/ngif.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ngif',
+  templateUrl: './ngif.component.html',
+  styleUrls: ['./ngif.component.css']
+})
+export class NgifComponent implements OnInit {
+  constructor() { }
+  ngOnInit(): void {
+  }
+
+  message = 'I\'m read only!';
+  canEdit = false;
+ 
+  onEditClick() {
+    this.canEdit = !this.canEdit;
+    if (this.canEdit) {
+      this.message = 'You can edit me!';
+    } else {
+      this.message = 'I\'m read only!';
+    }
+  }
+
+}
